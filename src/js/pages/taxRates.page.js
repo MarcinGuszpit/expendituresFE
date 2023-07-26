cbbb5a8dea1e92decfb1f203f165978cac137858
MarcinRG <marcin.guszpit@gmail.com>
2023-07-26 15:26:08 +0200

    ch

--- src/js/pages/taxRates.page.js
+++ src/js/pages/taxRates.page.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Box from "@mui/material/Box";
+import { TextField, InputAdornment, RadioGroup, FormControlLabel, Radio, FormControl } from "@mui/material";
 import { PageTitle } from "../components/other/pageTitle.component";
 
 export const TaxRatesPage = () => {
@@ -8,6 +9,23 @@ export const TaxRatesPage = () => {
             <Box sx={{ padding: '20px' }}>
                 <PageTitle>Stawki podatku</PageTitle>
             </Box>
+            <Box>
+                <form style={{  backgroundColor: 'white',borderLeft: '7px solid #aa9588', padding: '20px', margin: '20px'}}>
+                    <TextField id="tax-rate-name" label="Nazwa stawki" variant="outlined" fullWidth  margin="normal"/>
+                    <TextField id="tax-rate" label="Wartość stawki" variant="outlined" fullWidth margin="normal"  InputProps={{
+                        startAdornment: <InputAdornment position="start">%</InputAdornment>,}}/>
+                    <FormControl>
+                        <RadioGroup row  aria-labelledby="group-select" name="default">
+                        <FormControlLabel
+                            value="start"
+                            control={<Radio />}
+                            label="domyślna stawka"
+                            labelPlacement="start"/>
+                        </RadioGroup>   
+                    </FormControl>
+                </form>
+            </Box> 
+
         </React.Fragment>
     )
 }
